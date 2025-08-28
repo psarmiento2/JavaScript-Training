49b8357cf572212acbf76d480c67622a8078959a
psarmiento2 <psarmiento2@academic.rrc.ca>
2025-08-28 10:45:50 -0500

    Created sandbox,js and completed the javascript training video part 2 syntax and basic types

---
+++ sandbox.js
@@ -0,0 +1,148 @@
+alert ('hello, world');
+
+let age = 25; 
+let year = 2019
+
+console.log(age, year);
+
+age = 30;
+console.log(age);
+
+const points = 100;
+console.log(points);
+
+var score = 75;
+console.log(scrore);
+
+/* ----------------------------------------------------------------------------*/
+
+// // strings 
+// console.log('hello, world');
+
+// let email = 'peterpark@spiderman.com';
+// console.log(email);
+
+// // string concatenation (joing things together)
+// let firstName = 'Brandon';
+// let lastName = 'Sanderson';
+
+// let fullName = firstName + lastName;
+// console.log(fullName);
+
+// // getting characters 
+// console.log(fullName(2));
+
+// // string length
+// console.log(fullName.length);
+
+// // string methods
+
+// console.log(fullName.toUpperCase());
+// let result = fullName.toLowerCase();
+// console.log(result, firstName);
+
+// let index = email.indexOf('@');
+// console.log(index);
+
+// common string methods
+
+// let email = 'peterpark@spiderman.com';
+
+// let result = email.lastIndexOf('n');
+
+//let result = email.slice(0,10);
+
+//let result = email.substr(0,10);
+
+//let result = email.replace('m', 'w');
+
+//let result = email.replace('n', 'w');
+
+// console.log(result);
+
+/* ----------------------------------------------------------------------------*/
+//let radius = 10;
+//const pi = 3.14;
+
+//console.log(radius, pi);
+
+// math operators +, -, *, /, **, %
+
+// console.log(10/2);
+// let result = radius % 3;
+//let result = pi * radius**2;
+
+//order of operation - B I T M A S
+
+//let result = 5 * (10-3)**2;
+
+//console.log(result);
+
+//let likes = 10;
+
+// likes = likes + 1;
+// likes ++
+// likes --
+
+// likes += 10;
+// likes -= 5;
+// likes *= 5;
+// likes /= 5;
+
+//console.log(likes);
+
+//NaN = not a number 
+
+//console.log(5 / 'hello');
+
+// let result = 'the blog has' + 'likes' + 'likes';
+// console.log(result);
+
+/* ----------------------------------------------------------------------------*/
+
+// template strings 
+const tittle  = 'Best reads of 2019';
+const author = 'Mario';
+const likes = 30;
+
+// concatenation way
+// let result = 'The blog called' + title + 'by' + author + 'has' + likes + 'likes';
+// console.log(result);
+
+// template string way
+// let result = 'The blog called $[title} by  ${author} has ${likes} likes';
+// console.log(result);
+
+// // creating html templates
+// let html = `
+//     <h2>${title}</h2>
+//     <p>By ${author}</p>
+//     <span>This blog has ${likes} likes</span>
+// `;
+
+// console.log(html);
+
+/* ----------------------------------------------------------------------------*/
+
+let ninjas = ['shaun', 'ryu', 'chun-li'];
+
+// ninjas[1] = 'ken';
+// console.log(ninjas[1]);
+
+// let ages = [20,25,30,35];
+// console.log(ages[2]);
+
+// let random = ['shaun', 'crystal', 30, 20];
+// console.log(random);
+
+//console.log (ninjas.length);
+
+// array methods
+
+// let result = ninjas.join (',');
+// let result = ninjas.indexOf('chun-li');
+// let result = ninjas.concat(['ken', 'crystal']);
+// let result = ninjas.push('ken');
+// result = ninjas.pop();
+
+console.log(result);
